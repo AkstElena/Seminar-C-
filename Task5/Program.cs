c650ad6ecf86b838ed57f96b264ad00c6417548c
Akst Elena <akstelena@yandex.ru>
2023-02-25 11:52:21 +0300

    Задачи 4 и 5

---
+++ Task5/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача №7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+// 	456 -> 6
+// 	782 -> 2
+// 	918 -> 8
+
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int div = 0;
+
+if (number >= 100 && number < 1000)
+{
+  div = number % 10;
+  Console.WriteLine("Последняя цифра числа");
+  Console.WriteLine(div);
+}
+else
+{
+  Console.WriteLine("Обшибка ввода! Необходимо ввести трехзначное число");
+}
