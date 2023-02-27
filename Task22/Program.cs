1479b63f0065f5a16d8892e4b406757734449da7
Akst Elena <akstelena@yandex.ru>
2023-02-27 11:32:39 +0300

    C# Seminar2

---
+++ Task22/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+Random rand = new Random();
+int number = rand.Next(10, 1000);
+Console.WriteLine("Случайное число - это " + number);
+Console.WriteLine($"{number / 100}{number % 10}");
---
+++ Task24/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли первое число кратным второму. 
+// Если число 1 не кратно числу 2, то программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4 
+// 16, 4 -> кратно
+
+
+Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 % number2 == 0)
+  Console.WriteLine(number1 + " кратно " + number2);
+else
+{
+   Console.WriteLine(number1 + " некратно " + number2);
+   Console.WriteLine("Остаток от деления: " + (number1 % number2));
+}
+
+// string result = number1 % number2 == 0? "Второе число кратно первому":$"Второе число не кратно первому. Остаток от деления = {number1 % number2}";
