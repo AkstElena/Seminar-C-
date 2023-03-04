a17a124ef04475b68992a34d1b992ff5a3564663
Akst Elena <akstelena@yandex.ru>
2023-03-04 19:57:19 +0300

    Seminar 3

---
+++ Task31/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача №17. Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+int [] points = new int[2];
+Console.WriteLine("Ввод Х && У");
+
+for (int i = 0; i < points.Length; i = i + 1)
+points[i]= Convert.ToInt32(Console.ReadLine());
+
+if (points [0]> 0 && points [1] > 0)
+    Console.WriteLine("Это 1-ая четверть");
+else if (points [0]< 0 && points [1] > 0)
+    Console.WriteLine("Это 2-ая четверть");
+else if (points [0]< 0 && points [1] < 0)
+    Console.WriteLine("Это 3-ая четверть");
+else if (points [0]> 0 && points [1] < 0)
+    Console.WriteLine("Это 4-ая четверть");
+else  Console.WriteLine("Х && У = 0");
---
+++ Task34/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2Д пространстве
+
+int [] pointA = new int [2];
+int [] pointB = new int [2];
+
+
+Console.WriteLine("Введите х и у для точки А");
+for (int i = 0; i < pointA.Length; i = i + 1)
+pointA[i]= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите х и у для точки В");
+for (int i = 0; i < pointB.Length; i = i + 1)
+pointB[i]= Convert.ToInt32(Console.ReadLine());
+
+// double result = (((pointA[0]-pointB[0])^2 + (pointA[1]-pointB[1])^2))^(1/2);
+//Console.WriteLine("Расстояние между точкой А и В = " + result);
+
+double result = Math.Sqrt(Math.Pow(pointA[0]-pointB[0], 2) + Math.Pow(pointA[1]-pointB[1], 2));
+Console.WriteLine("Расстояние между точкой А и В = " + Math.Round(result, 2));
+
